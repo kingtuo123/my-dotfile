930ae38f7f16914335184807702324105772c331
kingtuo123 <kingtuo123@foxmail.com>
2026-02-22 11:44:42 +0800

    update

--- home/king/.config/nvim/init.lua
+++ home/king/.config/nvim/init.lua
@@ -14,152 +14,159 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
+
+
+
+
 vim.g.mapleader = " "         -- 设置空格为 <leader> 键 
 vim.opt.termguicolors = true  -- 启用真彩色支持
 
+
+
+
+
 require("lazy").setup({
 --------------------------------------------------[[  everforest 颜色主题 ]]--------------------------------------------------
-	{
-		"neanias/everforest-nvim",
-		version = false,
-		lazy = false,
-		priority = 1000,
-		config = function()
-			require("everforest").setup({
-				background = "medium",
-			})
-			require("everforest").load()
-		end,
-	},
+    {
+        "neanias/everforest-nvim",
+        version = false,
+        lazy = false,
+        priority = 1000,
+        config = function()
+            require("everforest").setup({
+                background = "medium",
+            })
+            require("everforest").load()
+        end,
+    },
 --------------------------------------------------[[     mini.nvim     ]]--------------------------------------------------
-	{
-		'echasnovski/mini.nvim',
-		version = false,
-		config = function()
-			require('mini.indentscope').setup({
-				draw = {delay = 100},
-				symbol = '|'
-			})
-		end,
-	},
+    {
+        'echasnovski/mini.nvim',
+        version = false,
+        config = function()
+            require('mini.indentscope').setup({
+                draw = {delay = 100},
+                symbol = '|'
+            })
+        end,
+    },
 --------------------------------------------------[[     页面平滑滚动     ]]--------------------------------------------------
-	{
-		"karb94/neoscroll.nvim",
-		config = function()
-			require('neoscroll').setup()
-		end
-	},
+    {
+        "karb94/neoscroll.nvim",
+        config = function()
+            require('neoscroll').setup()
+        end
+    },
 --------------------------------------------------[[     语法高亮优化     ]]--------------------------------------------------
-	{
-		"nvim-treesitter/nvim-treesitter", branch = 'master', lazy = false, build = ":TSUpdate"
-	},
+    {
+        "nvim-treesitter/nvim-treesitter", branch = 'master', lazy = false, build = ":TSUpdate"
+    },
 --------------------------------------------------[[    显示颜色#666666   ]]--------------------------------------------------
-	{
-		'brenoprata10/nvim-highlight-colors',
-		config = function() require('nvim-highlight-colors').setup({}) end
-	},
+    {
+        'brenoprata10/nvim-highlight-colors',
+        config = function() require('nvim-highlight-colors').setup({}) end
+    },
 --------------------------------------------------[[     文件搜索     ]]--------------------------------------------------
-	{
-		'nvim-telescope/telescope.nvim',
-		dependencies = { 'nvim-lua/plenary.nvim' },
-		config = function()
-			require('telescope').setup({
-				defaults = { 
-					initial_mode = "normal",
-					mappings = {
-						i = {
-							["<A-->"] = require('telescope.actions').select_horizontal,
-							["<A-\\>"] = require('telescope.actions').select_vertical,
-						},
-						n = {
-							["l"] = require('telescope.actions').select_default,
-							["<A-->"] = require('telescope.actions').select_horizontal,
-							["<A-\\>"] = require('telescope.actions').select_vertical,
-						}
-					},
-				},
-				pickers = {
-					find_files = {
-						theme = "dropdown",
-						previewer = false,
-					},
-					buffers = {
-						theme = "dropdown",
-						previewer = false,
-					},
-				}
-			})
-			local builtin = require('telescope.builtin')
-			vim.keymap.set('n', '<A-f>',      builtin.find_files, {})
-			vim.keymap.set('n', '<A-g>',      builtin.live_grep, {})
-			vim.keymap.set('n', '<A-b>',      builtin.buffers, {})
-			vim.keymap.set('n', '<A-h>',      builtin.help_tags, {})
-			vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-			vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-			vim.keymap.set('n', '<leader>b',  builtin.buffers, {})
-			vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
-		end
-	},
+    {
+        'nvim-telescope/telescope.nvim',
+        dependencies = { 'nvim-lua/plenary.nvim' },
+        config = function()
+            require('telescope').setup({
+                defaults = { 
+                    initial_mode = "normal",
+                    mappings = {
+                        i = {
+                            ["<A-->"] = require('telescope.actions').select_horizontal,
+                            ["<A-\\>"] = require('telescope.actions').select_vertical,
+                        },
+                        n = {
+                            ["l"] = require('telescope.actions').select_default,
+                            ["<A-->"] = require('telescope.actions').select_horizontal,
+                            ["<A-\\>"] = require('telescope.actions').select_vertical,
+                        }
+                    },
+                },
+                pickers = {
+                    find_files = {
+                        theme = "dropdown",
+                        previewer = false,
+                    },
+                    buffers = {
+                        theme = "dropdown",
+                        previewer = false,
+                    },
+                }
+            })
+            local builtin = require('telescope.builtin')
+            vim.keymap.set('n', '<A-f>',      builtin.find_files, {})
+            vim.keymap.set('n', '<A-g>',      builtin.live_grep, {})
+            vim.keymap.set('n', '<A-b>',      builtin.buffers, {})
+            vim.keymap.set('n', '<A-h>',      builtin.help_tags, {})
+            vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+            vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+            vim.keymap.set('n', '<leader>b',  builtin.buffers, {})
+            vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+        end
+    },
 --------------------------------------------------[[     显示搜索索引     ]]--------------------------------------------------
-	{ 
-		"kevinhwang91/nvim-hlslens",
-		config = function ()
-			require("hlslens").setup({
-				calm_down = true,			  -- 光标移出匹配项后取消高亮
-				nearest_only = true,          -- 只显示当前匹配项的索引
-				nearest_float_when = 'auto'   -- 浮动窗口显示时机: 'always', 'auto'
-			})
-			local kopts = {noremap = true, silent = true}
-			vim.api.nvim_set_keymap('n', 'n', [[<Cmd>execute('normal! ' . v:count1 . 'n')<CR><Cmd>lua require('hlslens').start()<CR>]], kopts)
-			vim.api.nvim_set_keymap('n', 'N', [[<Cmd>execute('normal! ' . v:count1 . 'N')<CR><Cmd>lua require('hlslens').start()<CR>]], kopts)
-			vim.api.nvim_set_keymap('n', '*', [[*<Cmd>lua require('hlslens').start()<CR>]], kopts)
-			vim.api.nvim_set_keymap('n', '#', [[#<Cmd>lua require('hlslens').start()<CR>]], kopts)
-			vim.api.nvim_set_keymap('n', 'g*', [[g*<Cmd>lua require('hlslens').start()<CR>]], kopts)
-			vim.api.nvim_set_keymap('n', 'g#', [[g#<Cmd>lua require('hlslens').start()<CR>]], kopts)
-		end
-	},
+    {
+        "kevinhwang91/nvim-hlslens",
+        config = function ()
+            require("hlslens").setup({
+                calm_down = true,			  -- 光标移出匹配项后取消高亮
+                nearest_only = true,          -- 只显示当前匹配项的索引
+                nearest_float_when = 'auto'   -- 浮动窗口显示时机: 'always', 'auto'
+            })
+            local kopts = {noremap = true, silent = true}
+            vim.api.nvim_set_keymap('n', 'n', [[<Cmd>execute('normal! ' . v:count1 . 'n')<CR><Cmd>lua require('hlslens').start()<CR>]], kopts)
+            vim.api.nvim_set_keymap('n', 'N', [[<Cmd>execute('normal! ' . v:count1 . 'N')<CR><Cmd>lua require('hlslens').start()<CR>]], kopts)
+            vim.api.nvim_set_keymap('n', '*', [[*<Cmd>lua require('hlslens').start()<CR>]], kopts)
+            vim.api.nvim_set_keymap('n', '#', [[#<Cmd>lua require('hlslens').start()<CR>]], kopts)
+            vim.api.nvim_set_keymap('n', 'g*', [[g*<Cmd>lua require('hlslens').start()<CR>]], kopts)
+            vim.api.nvim_set_keymap('n', 'g#', [[g#<Cmd>lua require('hlslens').start()<CR>]], kopts)
+        end
+    },
 --------------------------------------------------[[      底部状态栏      ]]--------------------------------------------------
-	{
-		'nvim-lualine/lualine.nvim',
-		dependencies = { 'nvim-tree/nvim-web-devicons' },
-		config = function()
-			require('lualine').setup({
-				options = {
-					theme = 'auto',
-					icons_enabled = false,
-					globalstatus = true, -- 全局状态栏，有多个窗口时只显示最底下的状态栏
-				},
-				sections = {
-					lualine_a = {'mode'},
-					--lualine_b = {'filename', path = { 1 }, },
-					lualine_b = {
-						{ 'filename', path = 1 } -- 显示如 "lua/config/lualine.lua"
-					},
-					lualine_c = {},
-					lualine_x = {},
-					lualine_y = {'progress'},
-					lualine_z = {'location'},
-				}
-			})
-		end
-	},
+    {
+        'nvim-lualine/lualine.nvim',
+        dependencies = { 'nvim-tree/nvim-web-devicons' },
+        config = function()
+            require('lualine').setup({
+                options = {
+                    theme = 'auto',
+                    icons_enabled = false,
+                    globalstatus = true, -- 全局状态栏，有多个窗口时只显示最底下的状态栏
+                },
+                sections = {
+                    lualine_a = {'mode'},
+                    --lualine_b = {'filename', path = { 1 }, },
+                    lualine_b = {
+                        { 'filename', path = 1 } -- 显示如 "lua/config/lualine.lua"
+                    },
+                    lualine_c = {},
+                    lualine_x = {},
+                    lualine_y = {'progress'},
+                    lualine_z = {'location'},
+                }
+            })
+        end
+    },
 --------------------------------------------------[[ 高亮当前窗口的分界线 ]]--------------------------------------------------
-	{
-	  "nvim-zh/colorful-winsep.nvim",
-	  config = true,
-	  event = { "WinLeave" },
-	},
+    {
+        "nvim-zh/colorful-winsep.nvim",
+        config = true,
+        event = { "WinLeave" },
+    },
 })
+------------------------------------------------------------------------------------------------------------------------------
+
 
 
 
 vim.opt.title          = true           -- 启用标题
-vim.opt.titlestring    = "nvim @ %F %m"    -- 标题格式
+vim.opt.titlestring    = "nvim @ %F %m" -- 标题格式
 vim.opt.number         = true           -- 显示行号
 vim.opt.relativenumber = true           -- 相对行号
-vim.opt.expandtab      = false          -- 禁止替换 tab 为空格
-vim.opt.shiftwidth     = 4              -- 缩进宽度
-vim.opt.tabstop        = 4              -- tab 显示宽度
 vim.opt.autoindent     = true           -- 自动缩进
 vim.opt.wrap           = false          -- 超出屏幕宽度的代码是否可见
 vim.opt.cursorline     = true           -- 高亮光标所在行
@@ -177,19 +184,43 @@ vim.cmd('filetype plugin indent on')                -- 禁止换行自动注释-
 vim.cmd('autocmd FileType * set formatoptions-=ro') -- 禁止换行自动注释-2
 
 
+
+
+
+vim.opt.list = true  -- 启用特殊字符显示
+vim.opt.listchars = {
+  tab = "⟩‐",        -- tab 键 '—'，必须两个字符，例如 “->” 其中 - 为第一个显示的字符，> 为循环显示的字符，若 tab 宽度为 4 ，则显示为 ->>>
+  trail = "␣",       -- 末尾空格
+  extends = "⟩",     -- 文字超出窗口右侧提示符
+  precedes = "⟨",    -- 文字超出窗口左侧提示符
+}
+
+
+
+
+
+-- tab 键配置
+vim.opt.expandtab      = true           -- 替换 tab 为空格
+vim.opt.tabstop        = 4              -- 真实的 tab 制表符显示宽度为 4 个空格
+vim.opt.shiftwidth     = 4              -- 替换后的 tab 缩进宽度为 4 个空格
+vim.opt.softtabstop    = 4              -- 退格键删除 4 个空格的宽度
+-- makefile 文件不替换 tab 为空格
 vim.api.nvim_create_autocmd("FileType", {
-  pattern = "*",
-  callback = function()
-    vim.opt_local.expandtab = false    -- 使用真正的制表符
-    vim.opt_local.tabstop = 4          -- 制表符宽度为4
-    vim.opt_local.shiftwidth = 4       -- 缩进宽度为4
-    vim.opt_local.softtabstop = 4      -- 退格键删除4个空格
-  end
+    pattern = "make",
+    callback = function()
+        vim.opt_local.expandtab = false
+    end
 })
 
 
+
+
+
+-- 非聚焦窗口背景色变暗
 --vim.api.nvim_set_hl(0, 'NormalNC', { bg = '#232a2e', blend = 100 }) -- 非聚焦窗口（Not Current）背景色，hex+透明度（blend）
 --vim.opt.winhighlight = 'Normal:Normal,NormalNC:NormalNC'
+
+
 
 
 
@@ -241,11 +272,14 @@ vim.keymap.set({'i','n'}, "<ESC>", "<ESC><cmd>lua fcitx()<CR>")
 
 
 
+
 -- 使用 # 搜索时不自动跳转
 vim.keymap.set("n", "#", function()
 	vim.fn.setreg("/", vim.fn.expand("<cword>"))
 	vim.cmd("set hlsearch")
 end)
+
+
 
 
 
@@ -257,23 +291,3 @@ function OpenInTerminal()
     os.execute("swaymsg splitv && foot -D " .. current_path .. " &>/dev/null && swaymsg split none &")
 end
 vim.keymap.set({'n', 'i'}, '<A-t>', OpenInTerminal)
-
-
-
-
-vim.api.nvim_create_user_command(
-	"Itable",
-	function(args)
-		local custom_text = {
-			"<div class=\"table-container\"> ",
-			"",
-			"|||",
-			"|:--|:--|",
-			"|||",
-			"",
-			"</div>"
-		}
-		vim.api.nvim_put(custom_text, "c", true, true)
-	end,
-	{nargs = 0}
-)
