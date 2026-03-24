234b478d6f2f9b02f7a578969636a1ab403b2225
kingtuo123 <kingtuo123@foxmail.com>
2026-03-24 13:10:02 +0800

    update

--- home/king/.config/i3blocks/scripts/temp.sh
+++ home/king/.config/i3blocks/scripts/temp.sh
@@ -1,11 +1,35 @@
 #!/bin/bash
 
-file_temp="/sys/class/hwmon/hwmon5/temp1_input"
+shopt -s nullglob
+
+
+names=(/sys/class/hwmon/hwmon*/name)
+
+
+if [[ ${#names[@]} -eq 0 ]]; then
+    echo "  未找到文件1  "
+    exit 1;
+fi
+
+
+for i in "${names[@]}"; do
+    if [[ $(<"$i") == "k10temp" ]]; then
+        file_temp="${i%/*}/temp1_input"
+    fi
+done
+
+
+if [[ -z "$file_temp" ]]; then
+    echo "  未找到文件2  "
+    exit 2;
+fi
+
+
 
 while true; do
-	temp=$(<$file_temp)
-	temp=$((temp/1000))
-	[[ $temp -ge 70 ]] && color="#e67e80" || color="#d3c6aa"
-	printf '{"full_text":"   温度  %s℃    ", "color":"%s"}\n' $temp  $color
-	sleep 2
+    temp=$(<$file_temp)
+    temp=$((temp/1000))
+    [[ $temp -ge 70 ]] && color="#e67e80" || color="#d3c6aa"
+    printf '{"full_text":"   温度  %s℃    ", "color":"%s"}\n' $temp  $color
+    sleep 2
 done
