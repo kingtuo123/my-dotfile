4a3a293627f9a8d61bfbe74720e1d1fb74dfed42
kingtuo123 <kingtuo123@foxmail.com>
2026-07-21 01:25:22 +0800

    update

--- home/king/.config/i3blocks/scripts/portage.sh
+++ home/king/.config/i3blocks/scripts/portage.sh
@@ -8,6 +8,11 @@ now=$(date +%s)
 diff_seconds=$((target - now))
 diff_days=$((diff_seconds / 86400))
 
-[[ ${diff_days#-} -ge 15 ]] && color="#e67e80" || color="#d3c6aa"
+if [[ ${diff_days#-} -ge 15 ]]; then
+    color="#e67e80"
+else
+    exit 1
+    color="#d3c6aa"
+fi
 
 printf '{"full_text":"   仓库更新于 %s 天前   ", "color":"%s"}\n' ${diff_days#-}  $color
